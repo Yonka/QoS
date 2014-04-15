230924695ab74fd2ec3e0a08afaec32a1e7f1983
Yonka <yonkaps@gmail.com>
2014-04-15 16:37:11 +0400

    -

--- trafgen.cpp
+++ trafgen.cpp
@@ -1,6 +1,7 @@
 #include "trafgen.h"
 #include "cstdlib"
 #include "ctime"
+#include <vector>
 
 trafgen::trafgen(sc_module_name mn, int param = 1) : sc_module(mn), runs(param)
 {
@@ -13,14 +14,19 @@ void trafgen::gen_event()
 {
     for (int r = 0; r < runs; r++)
     {
+        vector<sc_uint<8> > packet;
         for (int i = 0; i < 19; i++)
         {
-            x = (sc_uint<8>)rand() % 255;
+            //x = (sc_uint<8>)rand() % 255;
+            packet.push_back((sc_uint<8>)rand() % 255);
             send_byte();
             wait();
         }
         x = 255;
         send_byte();
+        send_packet();
+        if (отправили_НЕ_последний_байт_пакета)
+            send.notify(timeDelay);
         wait();
     }
 }
